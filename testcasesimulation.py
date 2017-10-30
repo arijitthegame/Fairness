d75c3d174374744b156e69dd61b2fd9bfec363b8
arijitthegame <arijitthegame@gmail.com>
2017-10-30 10:42:45 -0400

    Update testcasesimulation.py

--- testcasesimulation.py
+++ testcasesimulation.py
@@ -1,4 +1,16 @@
-import numpy as np
-import random
-for i in range (50)
-listofvotes.appends=random.uniform(.3,.9)
+import pylab
+import matplotlib.pyplot as plt
+def make_data():
+  s=np.random.uniform(.3,.85,50)
+  x=np.average(s)
+  y=np.array(filter(lambda x:x>=0.5,s)).size
+  print "%.5f,%.5f" %(x,y)
+  return (x,y)
+
+data=[make_data() for i in xrange(100)]
+zip(*data)
+
+plt.scatter(*zip(*data))
+plt.show()
+
+
