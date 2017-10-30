cf398e7d0953688526e5028beda531f2f4a1eb2d
arijitthegame <arijitthegame@gmail.com>
2017-10-30 11:01:58 -0400

    Update testcasesimulation.py

--- testcasesimulation.py
+++ testcasesimulation.py
@@ -10,7 +10,16 @@ def make_data():
 data=[make_data() for i in xrange(100)]
 zip(*data)
 
-plt.scatter(*zip(*data))
-plt.show()
+fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(*zip(*data))
+        ax.xaxis.set_ticks(np.arange(0,1,.1))
+        ax.yaxis.set_ticks(np.arange(0,50,10))
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_title('Fairness')
+        plt.savefig('Fairness.png')
+        fig.show()
+
 
 
